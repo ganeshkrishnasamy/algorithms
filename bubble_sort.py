1dcb6c1402a9b1b93326bc37e678856d12123c41
ganeshkrishnasamy <33069264+ganeshkrishnasamy@users.noreply.github.com>
2017-10-30 11:42:47 -0400

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,18 @@
 # Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
 # Runs O(n^2) time even if the array is sorted. It can be optimized by stopping the algorithm if inner loop didn’t cause any swap
 
+#Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+
+#Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+#Auxiliary Space: O(1)
+
+#Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+
+#Sorting In Place: Yes
+
+#Stable: Yes
+
 def bubble_sort(arr):
     for i in range(len(arr)):
         swap=False
